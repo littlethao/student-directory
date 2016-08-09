3ae6263e4d4042c15ae710334c3d9a31aba778bb
Thao Vo <ttthao.vo@gmail.com>
2016-08-09 18:04:30 +0100

    Added load_students method according to step 12

--- directory.rb
+++ directory.rb
@@ -1,38 +1,39 @@
 @students = [] # an empty array accessible to all methods
 
 def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
-  # get the first name
-  name = gets.chomp
-  # while the name is not empty, repeat this code
-  while !name.empty? do
-    # add the student hash to the array
-    @students << {name: name, cohort: :november}
-    puts "Now we have #{@students.count} students"
-    # get another name from the user
-    name = gets.chomp
-  end
+	puts "Please enter the names of the students"
+	puts "To finish, just hit return twice"
+	# get the first name
+	name = gets.chomp
+	# while the name is not empty, repeat this code
+	while !name.empty? do
+		# add the student hash to the array
+		@students << {name: name, cohort: :november}
+		puts "Now we have #{@students.count} students"
+		# get another name from the user
+		name = gets.chomp
+	end
 end
 
 def interactive_menu
-  loop do
-    print_menu
-    process(gets.chomp)
-  end
+	loop do
+		print_menu
+		process(gets.chomp)
+	end
 end
 
 def print_menu
-  puts "1. Input the students"
-  puts "2. Show the students"
-  puts "3. Save the list to students.csv"
-  puts "9. Exit" # 9 because we'll be adding more items
+	puts "1. Input the students"
+	puts "2. Show the students"
+	puts "3. Save the list to students.csv"
+	puts "4. Load the list from students.csv"
+	puts "9. Exit" # 9 because we'll be adding more items
 end
 
 def show_students
-  print_header
-  print_student_list
-  print_footer
+	print_header
+	print_student_list
+	print_footer
 end
 
 def process(selection)
@@ -43,6 +44,8 @@ def process(selection)
 		show_students
 	when "3"
 		save_students
+	when "4"
+		load_students
 	when "9"
 		exit # this will cause the program to terminate
 	else
@@ -51,18 +54,18 @@ def process(selection)
 end
 
 def print_header
-  puts "The students of Villains Academy"
-  puts "-------------"
+	puts "The students of Villains Academy"
+	puts "-------------"
 end
 
 def print_student_list
-  @students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
-  end
+	@students.each do |student|
+		puts "#{student[:name]} (#{student[:cohort]} cohort)"
+	end
 end
 
 def print_footer
-  puts "Overall, we have #{@students.count} great students"
+	puts "Overall, we have #{@students.count} great students"
 end
 
 def save_students
@@ -73,6 +76,15 @@ def save_students
 		student_data = [student[:name], student[:cohort]]
 		csv_line = student_data.join(",")
 		file.puts csv_line
+	end
+	file.close
+end
+
+def load_students
+	file = File.open("students.csv", "r")
+	file.readlines.each do |line|
+	name, cohort = line.chomp.split(',')
+		@students << {name: name, cohort: cohort.to_sym}
 	end
 	file.close
 end
